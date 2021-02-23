f809ded7c0ae11d4e330fac234fe6da3928f74f8
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-02-23 10:46:51 -0300

    chore: initial project structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { ExperienceBar } from '../components/ExperienceBar';
+
+function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar/>
+    </div>
+  );
+  
+}
+
+export default Home;
